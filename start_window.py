144711bcb4d4cea15fc65915f2edce5c1bb6b021
TiGeLi146 <t204311@gmail.com>
2023-01-15 16:51:06 +0500

    Added start window

---
+++ start_window.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+import pygame
+
+FPS = 50
+
+size = WIDTH, HEIGHT = 1280, 720
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+pygame.init()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print("Error")
+        raise SystemExit(message)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def main():
+    bg = pygame.transform.scale(load_image('bg.jpg'), (WIDTH, HEIGHT))
+    screen.blit(bg, (0, 0))
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    main()
